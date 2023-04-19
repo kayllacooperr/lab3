c1b0945cf2569adc259707540a1f9a0d83bb3da8
Kayla Rebecca Cooper <krcooper@ucsd.edu>
2023-04-19 15:44:31 -0700

    Checkpoint

--- ArrayTests.java
+++ ArrayTests.java
@@ -9,10 +9,22 @@ public class ArrayTests {
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
 
-
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void reversedTest2(){
+    int[] test1 = {1,2,3};
+    assertArrayEquals(new int[]{3,2,1}, ArrayExamples.reversed(test1));
+  }
+
+  @Test
+  public void reversedInPlace2(){
+    int[] test2 = {4,5,6};
+    ArrayExamples.reverseInPlace(test2);
+    assertArrayEquals(new int[] {6,5,4}, test2);
   }
 }
